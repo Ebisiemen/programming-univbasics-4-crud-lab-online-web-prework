f6f5727043601efe44b476bec40510b4a55d6f43
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-07-26 23:54:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -36,7 +36,7 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-  array[index_number] = element learn 
+  array[index_number] = element 
 end
 
 binding.pry
